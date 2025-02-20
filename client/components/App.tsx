14bb7351f52a83275b83c3a4f104158888164f63
Liam Simpson <contact@liamsimpson.co.nz>
2025-02-21 10:55:50 +1300

    completing base level styling

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
-import { locationsArray } from '../locations.ts'
-import { activitiesArray } from '../activities.ts'
-import { clothing } from '../clothing.ts'
-import { food } from '../food.ts'
-import { gear } from '../gear.ts'
+import { locationsArray } from '../data/locations.ts'
+import { activitiesArray } from '../data/activities.ts'
+import { clothing } from '../data/clothing.ts'
+import { food } from '../data/food.ts'
+import { gear } from '../data/gear.ts'
 
 function App() {
   //STATE
@@ -29,7 +29,7 @@ function App() {
   if (startDate && endDate) {
     const timeDifference = endDate.getTime() - startDate.getTime()
     const duration = Math.round(timeDifference / (1000 * 3600 * 24))
-    displayedDuration = 'duration: ' + duration + ' nights'
+    displayedDuration = 'Duration: ' + duration + ' nights'
 
     filteredFood = food.filter((i) => i.tripDuration === duration)
   } else {
@@ -60,132 +60,137 @@ function App() {
 
   return (
     <>
-      <div>
-        <header className="place-items-center">
-          <h1 className="text-red-100">PAKT</h1>
-        </header>
-        <main>
-          <section className="bg-[#38473E] rounded-[5vw] place-items-center">
-            <form className="grid grid-cols-9">
-              <p>Im travelling to</p>
-              <div>
-                <select
-                  value={location}
-                  onChange={handleLocationChange}
-                  aria-label="location"
-                  className="rounded-full bg-[#FFCB10] text-[#38473E]"
-                >
-                  {locationsArray.map((location, index) => (
-                    <option key={index} value={location.name}>
-                      {location.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
+      <header className="pl-16">
+        <h1>PAKT</h1>
+      </header>
+      <main className="mx-8 mb-8">
+        <div className="bg-[#38473E] rounded-[20px]">
+          <p className="p-8 mb-8 text-[#e7e9de]">
+            Designed to streamline the packing process for the busy adventurer.{' '}
+            <br />
+            PAKT helps you get started with fast templated gear lists catered to
+            the weather conditions and length of your trip.
+            <br />
+            Tell us about your trip below to get started.
+          </p>
+        </div>
+        <div className="bg-[#38473E] rounded-[20px]">
+          <form className="grid grid-cols-2 lg:grid-cols-4 p-8 mb-8">
+            <p className="mr-1">Location:</p>
 
-              <p>for a</p>
-              <div>
-                <select
-                  value={activity}
-                  onChange={handleActivityChange}
-                  className="rounded-full bg-[#FFCB10] text-[#38473E]"
-                >
-                  {activitiesArray.map((activity, index) => (
-                    <option key={index} value={activity.name}>
-                      {activity.name}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <p>trip.</p>
+            <select
+              value={location}
+              onChange={handleLocationChange}
+              aria-label="location"
+              className="bg-[#38473E] text-[#e7e9de] mr-8 italic"
+            >
+              {locationsArray.map((location, index) => (
+                <option key={index} value={location.name}>
+                  {location.name}
+                </option>
+              ))}
+            </select>
+            <p className="mr-1">Activity:</p>
 
-              <p>Ill be there from the</p>
-              <DatePicker
-                placeholderText="Select start date"
-                selected={startDate}
-                selectsStart
-                onChange={handleStartDateChange}
-                dateFormat="dd/MM/yyyy"
-                startDate={startDate}
-                className="rounded-full bg-[#FFCB10] text-[#38473E]"
-              />
-              <p>to the</p>
-              <DatePicker
-                placeholderText="Select end date"
-                selected={endDate}
-                selectsEnd
-                startDate={startDate}
-                endDate={endDate}
-                minDate={startDate}
-                onChange={handleEndDateChange}
-                dateFormat="dd/MM/yyyy"
-                className="rounded-full bg-[#FFCB10] text-[#38473E]"
-              />
-            </form>
-          </section>
+            <select
+              value={activity}
+              onChange={handleActivityChange}
+              className="bg-[#38473E] text-[#e7e9de] mr-8 italic"
+            >
+              {activitiesArray.map((activity, index) => (
+                <option key={index} value={activity.name}>
+                  {activity.name}
+                </option>
+              ))}
+            </select>
 
-          {location && (
-            <section className="bg-[#38473E] rounded-[5vw]">
-              <div className="flex">
-                {location && (
-                  <div className="justify-start">
-                    <p>
-                      {location} {activity} Trip
-                    </p>
+            <p className="mr-2">Duration:</p>
+            <DatePicker
+              placeholderText="Select start date"
+              selected={startDate}
+              selectsStart
+              onChange={handleStartDateChange}
+              dateFormat="dd/MM/yyyy"
+              startDate={startDate}
+              className="bg-[#38473E] text-[#e7e9de] italic"
+            />
+            <p className="mr-2">to</p>
+            <DatePicker
+              placeholderText="Select end date"
+              selected={endDate}
+              selectsEnd
+              startDate={startDate}
+              endDate={endDate}
+              minDate={startDate}
+              onChange={handleEndDateChange}
+              dateFormat="dd/MM/yyyy"
+              className="bg-[#38473E] text-[#e7e9de] italic"
+            />
+          </form>
+        </div>
+
+        {location && (
+          <div className="p-8 bg-[#38473E] rounded-[20px]">
+            <div>
+              {location && (
+                <>
+                  <div className="flex justify-between flex-wrap">
+                    <div>
+                      <h2>
+                        {location} {activity} Trip
+                      </h2>
+                    </div>
+
+                    <div>
+                      <p> Average Temperature: 15°</p>
+                      <p>{displayedDuration}</p>
+                    </div>
                   </div>
-                )}
+                </>
+              )}
+            </div>
 
-                {location && (
-                  <div className="justify-end">
-                    <p> Average Temperature: 15°</p>
-                  </div>
-                )}
-                <div className="justify-end">
-                  <p>{displayedDuration}</p>
-                </div>
-              </div>
+            <div>
+              {filteredGear.length > 0 && <h3 className="pb-2 pt-4">Gear</h3>}
+              <ul className="flex flex-wrap">
+                {filteredGear.length > 0 &&
+                  filteredGear.map((gear, index) => (
+                    <li key={index}>
+                      <h4 className="rounded-full px-4 mr-2 my-1.5 bg-[#e7e9de] text-[#38473E]">
+                        {gear.name}
+                      </h4>
+                    </li>
+                  ))}
+              </ul>
+              {filteredClothing.length > 0 && (
+                <h3 className="pb-2 pt-8">Clothing</h3>
+              )}
 
-              <div>
-                {filteredGear.length > 0 && <h3>Gear</h3>}
-                <ul className="flex">
-                  {filteredGear.length > 0 &&
-                    filteredGear.map((gear, index) => (
-                      <li key={index}>
-                        <p className="rounded-full bg-[#FFCB10] text-[#38473E]">
-                          {gear.name}
-                        </p>
-                      </li>
-                    ))}
-                </ul>
-                {filteredClothing.length > 0 && <h3>Clothing</h3>}
-
-                <ul className="flex">
-                  {filteredClothing.length > 0 &&
-                    filteredClothing.map((clothing, index) => (
-                      <li key={index}>
-                        <p className="rounded-full bg-[#FFCB10] text-[#38473E]">
-                          {clothing.name}
-                        </p>
-                      </li>
-                    ))}
-                </ul>
-                {filteredFood.length > 0 && <h3>Food</h3>}
-                <ul className="flex">
-                  {filteredFood.length > 0 &&
-                    filteredFood.map((food, index) => (
-                      <li className="pr-6" key={index}>
-                        <p className="rounded-full bg-[#FFCB10] text-[#38473E]">
-                          {food.name}
-                        </p>
-                      </li>
-                    ))}
-                </ul>
-              </div>
-            </section>
-          )}
-        </main>
-        <footer></footer>
-      </div>
+              <ul className="flex flex-wrap">
+                {filteredClothing.length > 0 &&
+                  filteredClothing.map((clothing, index) => (
+                    <li key={index}>
+                      <h4 className="rounded-full px-4 mr-2 my-1.5 bg-[#e7e9de] text-[#38473E]">
+                        {clothing.name}
+                      </h4>
+                    </li>
+                  ))}
+              </ul>
+              {filteredFood.length > 0 && <h3 className="pb-2 pt-8">Food</h3>}
+              <ul className="flex flex-wrap">
+                {filteredFood.length > 0 &&
+                  filteredFood.map((food, index) => (
+                    <li key={index}>
+                      <h4 className="rounded-full px-4 mr-2 my-1.5 bg-[#e7e9de] text-[#38473E]">
+                        {food.name}
+                      </h4>
+                    </li>
+                  ))}
+              </ul>
+            </div>
+          </div>
+        )}
+      </main>
     </>
   )
 }
