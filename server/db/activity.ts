abefd0ed2366c858be7666480bdf816c7373531a
Liam Simpson <contact@liamsimpson.co.nz>
2025-03-11 16:31:33 +1300

    creating db functions for activity

--- server/db/activity.ts
+++ server/db/activity.ts
@@ -1,19 +1,53 @@
+import { Activity } from 'models/models'
 import db from './connection'
 
-//GET ACTIVITES BY USERID
-export async function getActivities(userId) {
+//GET ACTIVITY BY USERID
+export async function getActivities(userId: number) {
   try {
-    const activities = await db('activity').where('user_id', userId)
+    const activities = await db('activity').where('user_id', userId).select()
     return activities
   } catch (error) {
     throw new Error(
-      `Error getting activities associated to userId: ${userId}, ${error}`,
+      `Error getting activities associated to userId: ${userId}. ${error}`,
     )
   }
 }
 
-//CREATE ACTIVITY BY USERID
+//CREATE ACTIVITY
+export async function createActivity(data: Activity, userId: number) {
+  try {
+    const { id, name } = data
+    const createdActivity = await db('activity').insert({
+      id,
+      name,
+      user_id: userId,
+    })
+    return createdActivity
+  } catch (error) {
+    throw new Error(
+      `Error adding activity with data: ${data}, associated to userId: ${userId}. ${error}.`,
+    )
+  }
+}
 
 //DELETE ACTIVITY BY USERID
+export async function deleteActivity(activityId: number) {
+  try {
+    const deletedActivity = await db('activity').where({ activityId }).del()
+    return deletedActivity
+  } catch (error) {
+    throw new Error(`Error deleting activity: ${activityId}`)
+  }
+}
 
 //EDIT ACTIVITY BY USERID
+export async function editActivity(data: Activity, activityId: number) {
+  try {
+    const editedActivty = await db('activity')
+      .where({ activityId })
+      .update({ data })
+    return editedActivty
+  } catch (error) {
+    throw new Error(`Error editing activity: ${activityId}`)
+  }
+}
